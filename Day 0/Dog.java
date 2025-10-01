4bb34252f47fe66c74ebdc09c95f11636e1fc3ec
rananda1 <rananda1@hwemail.com>
2025-10-01 09:14:56 -0700

    Worked October 1

---
+++ Day 0/Dog.java
@@ -0,0 +1,126 @@
+public class Dog {
+    
+    private String name;
+    private String ownerName;
+    private int age;
+    private int dogId;
+    private char dogChar;
+    private String dogTag;
+    private boolean stillInFacility;
+    
+    public int getAge() {
+         return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getDogChar() {
+        return dogChar;
+    }
+
+    public void setDogChar(char dogChar) {
+        this.dogChar = dogChar;
+    }
+
+    public int getDogId() {
+        return dogId;
+    }
+
+    public void setDogId(int dogId) {
+        this.dogId = dogId;
+    }
+
+    public String getDogTag() {
+        return dogTag;
+    }
+
+    public void setDogTag(String dogTag) {
+        this.dogTag = dogTag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public boolean getStillInFacility() {
+        return stillInFacility;
+    }
+
+    public void setStillInFacility(boolean stillInFacility) {
+        this.stillInFacility = stillInFacility;
+    }
+
+    public Dog(String name, String ownerName, int age, int dogId) {
+        this.name = name;
+        this.ownerName = ownerName;
+        this.age = age;
+        this.dogId = dogId;
+        this.dogChar = generateDogChar();
+        this.dogTag = generateDogTag();
+        this.stillInFacility = true;
+    }
+
+    public Dog() {
+        this.name = "dog";
+        this.ownerName = "Rose";
+        this.age = 5;
+        this.dogId = 123;
+        this.dogChar = generateDogChar();
+        this.dogTag = generateDogTag();
+        this.stillInFacility = true; 
+    }
+
+    public String toString() {
+        return name + " is a good dog. They are " + age 
+        + " years old and belong to " + ownerName + ". It is " 
+        + stillInFacility + " that they are still in our facility." + 
+        "For employee use only: DogTag is " + dogTag + ".";
+    }
+
+    public char generateDogChar() {
+
+    }
+
+    public String generateDogTag() {
+
+    }
+
+    public boolean equals(Dog other) {
+        if (this.toString().equals(other.toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
