2a203d63c2f6f3cd020a8f9127ec50c117db8112
rananda1 <rananda1@hwemail.com>
2025-10-30 19:47:04 -0700

    Oct 30

--- Day 0/Line.java
+++ Day 0/Line.java
@@ -4,6 +4,53 @@ public class Line {
     private int b;
     private int c;
 
+    private Point p1;
+    private Point p2;
+
+    public Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+        // instance variables a, b, and c
+        // a = - (y2 - y1)
+        this.a = -(p2.getY() - p1.getY()); 
+
+        // b = x2 - x1
+        this.b = p2.getX() - p1.getX(); 
+
+        // c = - (a * x1 + b * y1)
+        this.c = -(this.a * p1.getX() + p1.getY() * this.b);
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public void setP2(Point p2) {
+        this.p2 = p2;
+    }
+
+
+
     public int getA() {
         return a;
     }
@@ -24,15 +71,33 @@ public class Line {
 
     public double calculateSlope() {
         return (double) -1 * a / b;
+        //return (double) ((p1.getX() - p2.getX()) / (p1.getY() - p2.getY()));
     }
 
-    public boolean isCoordinateOnLine(int x, int y) {
-        if (((a * x) + (b * y) + c) == 0) {
+    public double calculateSlopeFromPoints() {
+        return (double) ((p1.getX() - p2.getX()) / (p1.getY() - p2.getY()));
+    }
+
+    //public boolean isCoordinateOnLine(int x, int y) {
+    //    //if (((a * x) + (b * y) + c) == 0) {
+    public boolean isCoordinateOnLine(Point p) {
+        if (((a * p.getX()) + (b * p.getY()) + c) == 0) {
             return true;
         } else {
             return false;
         }
     }
+
+    public boolean isOnLine(Point p) {
+        if (((a * p.getX()) + (b * p.getY()) + c) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //public double calculateSlope()
+
 
     public String toString() {
         return a + "x + " + b + "y + " + c + " = 0";
@@ -51,6 +116,18 @@ public class Line {
             return false; 
         }
     }
+
+
+    public String generatePointSlopeFormula() {
+        return "" + "(y - " + p1.getY() + ") = " 
+            + calculateSlope() + "(x - " + p1.getX() + ")";
+    }
+
+
+
+
+
+    
 
 
 
--- Day 0/LineTester.java
+++ Day 0/LineTester.java
@@ -1,23 +1,49 @@
 public class LineTester {
     public static void main(String[] args) {    
+        
+        Point p1 = new Point(2, 7);
+        Point p2 = new Point(1, 4);
+        Point p3 = new Point(1, 18);
+        Point p4 = new Point(4, 13);
+        
+        
         // Testing Line 1
         Line line1 = new Line(5, 4, -17);
         double slope1 = line1.calculateSlope(); // slope1 should be -1.25 
         // Should be true, as 5(5) + 4(-2) + (-17) = 0
-        boolean onLine1 = line1.isCoordinateOnLine(5, -2); 
+        //boolean onLine1 = line1.isCoordinateOnLine(5, -2);
+        boolean onLine1 = line1.isCoordinateOnLine(p1); 
 
         // Testing Line 2
         Line line2 = new Line(-25, 40, 30);
         double slope2 = line2.calculateSlope(); // slope2 should be 0.625
 
         // Should be false as -25(5) + 40(-2) + 30 != 0
-        boolean onLine2 = line2.isCoordinateOnLine(5, -2); 
+        //boolean onLine2 = line2.isCoordinateOnLine(5, -2); 
+        boolean onLine2 = line2.isCoordinateOnLine(p1); 
 
 
         System.out.println(slope1);
         System.out.println(onLine1);
         System.out.println(slope2);
         System.out.println(onLine2);
+
+
+
+        // Point p1 = new Point(2, 7);
+        // Point p2 = new Point(1, 4);
+        // Point p3 = new Point(1, 18);
+        // Point p4 = new Point(4, 13);
+
+        Line pointLine = new Line(p1, p2);
+
+        System.out.println("Using calculateClope(): " + pointLine.calculateSlope());
+
+        System.out.println("Is p1 on the line? " + pointLine.isOnLine(p1));
+        System.out.println("Is p2 on the line? " + pointLine.isOnLine(p2));
+        System.out.println("Is p3 on the line? " + pointLine.isOnLine(p3));
+        System.out.println("Is p4 on the line? " + pointLine.isOnLine(p4));
+
 
 
 
