08729281af251741edb141ccda0065fec2cad2fe
rananda1 <rananda1@hwemail.com>
2026-01-22 12:26:33 -0800

    Jan 22

---
+++ Day 0/MatrixFun.java
@@ -0,0 +1,85 @@
+public class MatrixFun {
+
+    private int[][] matrix;
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public MatrixFun(int numberOfRows, int numberOfCols) {
+        matrix = new int[numberOfRows][numberOfCols];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                matrix[r][c] = (int) (Math.random() * 10);
+            }
+        }
+    }
+
+    public MatrixFun(int[][] starterMatrix) {
+        matrix = starterMatrix;
+    }
+
+    public MatrixFun() {
+        this(3, 3);
+    }
+
+    public String toString() {
+        String matrixString = "";
+        for (int i = 0; i < matrix[0].length * 2 - 1; i++) {
+            matrixString += "=";
+        }
+        matrixString += "\n";
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                matrixString += matrix[r][c] + " ";
+            }
+            matrixString += "\n";
+        }
+        for (int i = 0; i < matrix[0].length * 2 - 1; i++) {
+            matrixString += "=";
+        }
+        return matrixString;
+    }
+
+    public boolean equals(MatrixFun other) {
+        if (this.toString().equals(other.toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean equals(int[][] other) {
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                if (this.matrix[r][c] != other[r][c]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    public void replaceAll(int oldValue, int newValue) {
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                if (matrix[r][c] == oldValue) {
+                    matrix[r][c] = newValue;
+                }
+            }
+        }
+    }
+
+    public void swapRow(int rowA, int rowB) {
+        // nt[][] same = this.matrix;
+        int[] same = matrix[rowA];
+        matrix[rowA] = matrix[rowB];
+        matrix[rowB] = same;
+    }
+
+}
---
+++ Day 0/MatrixFunTester.java
@@ -0,0 +1,29 @@
+public class MatrixFunTester {
+
+    public static void main(String[] args) {
+
+        MatrixFun matrix1 = new MatrixFun();
+
+        int[][] mat2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+        System.out.println(matrix1.toString());
+
+        System.out.println(matrix1.equals(mat2));
+
+        System.out.println(mat2.toString());
+
+        System.out.println(matrix1.toString());
+
+        matrix1.replaceAll(1, 9);
+
+        System.out.println(matrix1.toString());
+
+        matrix1.swapRow(0, 2);
+
+        System.out.println(matrix1.toString());
+
+        // System.out.println(matrix.equals(mat2));
+
+    }
+
+}
